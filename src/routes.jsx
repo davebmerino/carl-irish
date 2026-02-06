b7fc39ee678ef528f4dccf02edbd6cd47e499e35
davebmerino <daveb.merino@gmail.com>
2026-02-06 20:39:34 +0800

    routes

--- src/routes.jsx
+++ src/routes.jsx
@@ -1,6 +1,7 @@
 import { createBrowserRouter } from "react-router";
 import Rsvp from "./pages/rsvp/Rsvp.jsx";
 import Firstpage from "./pages/firstpage/Firstpage.jsx";
+import Landingpage from "./pages/firstpage/Landingpage.jsx";
 import Home from "./pages/homepage/Home.jsx";
 
 export const router = createBrowserRouter([
